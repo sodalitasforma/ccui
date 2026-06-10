feebc34e1f0cc03f4654a65ba149e2301bcb2a21
Sodalitas Forma <sodalitasforma@gmail.com>
2026-06-10 08:19:21 -0600

    Polish home header and add changelog page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,5 @@
+import { DocSearch } from "../../../../packages/primitives/src";
+import { docsSearchItems } from "./docs/search-index";
 import { ThemeTestToggle } from "./components/theme-test-toggle";
 import Image from "next/image";
 import systemStats from "./system-stats.json";
@@ -44,10 +46,22 @@ export default function Home() {
           <Cluster gap="md" align="center" className="home-nav-actions">
             <nav className="home-nav-links" aria-label="Primary navigation">
               <Link href="/docs">Docs</Link>
+              <Link href="/components-gallery" className="home-nav-link-desktop">Components</Link>
             </nav>
 
+            <div className="home-doc-search">
+              <DocSearch
+                items={docsSearchItems}
+                placeholder="Search docs..."
+                ariaLabel="Search documentation"
+              />
+            </div>
+
             <Link href="https://github.com/sodalitasforma/ccui" className="home-stars-link" aria-label="GitHub stars">
-              0
+              <svg className="home-stars-link__icon" viewBox="0 0 16 16" aria-hidden="true" focusable="false">
+                <path d="M8 0.2C3.6 0.2 0 3.8 0 8.2c0 3.5 2.3 6.5 5.5 7.6 0.4 0.1 0.5-0.2 0.5-0.4v-1.4c-2.2 0.5-2.7-1.1-2.7-1.1-0.4-0.9-0.9-1.1-0.9-1.1-0.7-0.5 0.1-0.5 0.1-0.5 0.8 0.1 1.2 0.8 1.2 0.8 0.7 1.2 1.9 0.9 2.3 0.7 0.1-0.5 0.3-0.9 0.5-1.1-1.8-0.2-3.6-0.9-3.6-3.9 0-0.9 0.3-1.6 0.8-2.1-0.1-0.2-0.4-1 0.1-2.1 0 0 0.7-0.2 2.2 0.8 0.6-0.2 1.3-0.3 2-0.3s1.4 0.1 2 0.3c1.5-1 2.2-0.8 2.2-0.8 0.4 1.1 0.2 1.9 0.1 2.1 0.5 0.6 0.8 1.3 0.8 2.1 0 3-1.8 3.7-3.6 3.9 0.3 0.3 0.6 0.8 0.6 1.6v2.4c0 0.2 0.1 0.5 0.6 0.4 3.2-1.1 5.5-4.1 5.5-7.6C16 3.8 12.4 0.2 8 0.2Z" />
+              </svg>
+              <span className="home-stars-link__count">0</span>
             </Link>
 
             <Button href="https://github.com/sodalitasforma/ccui/issues" size="sm">
